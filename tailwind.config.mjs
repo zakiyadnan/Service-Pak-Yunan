482ca991fbdc0dfda3636b1f8bb0f7165e01a012
ZakiyAdnanMahendra <zakiyadnan1411@gmail.com>
2026-01-22 15:09:07 +0700

    add Bing verification and location map

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,6 +11,7 @@ export default {
             },
             fontFamily: {
                 sans: ['Inter', 'system-ui', 'sans-serif'],
+                courgette: ['Courgette', 'cursive'],
             },
             animation: {
                 'float': 'float 3s ease-in-out infinite',
